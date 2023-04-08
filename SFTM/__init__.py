8be17fea2ab6f96f493253a38116061c2969cf09
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2023-04-08 22:32:50 +0700

    [modify]: change to socketio

--- SFTM/__init__.py
+++ SFTM/__init__.py
@@ -1,6 +1,33 @@
 from flask import Flask
-from SFTM.mqtt import *
+# from SFTM.mqtt import *
 from SFTM.routes import home_blueprint
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(home_blueprint)
+
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, engineio_logger=True,
+                    logger=True, cors_allowed_origins="*", cors_credentials=True)
+
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+
+@socketio.on('light')
+def handle_light(light_status):
+    print('Received light status:', light_status)
+
+
+@socketio.on('temperature')
+def handle_temperature(temp_status):
+    print('Received temperature status:', temp_status)
--- run.py
+++ run.py
@@ -1,4 +1,40 @@
-from SFTM import app
+import os
+from SFTM import app, socketio
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='localhost')
+    socketio.run(app, port=int(os.environ.get('PORT', 5000)))
+
+###############################################################
+# import socketio
+# import eventlet
+
+# sio = socketio.Server(cors_allowed_origins='*')
+# app = socketio.WSGIApp(sio)
+
+
+# @sio.event
+# def connect(sid, environ):
+#     print(sid, 'connected')
+#     sio.emit('connected_event', {'message': 'Client connected successfully'})
+
+
+# @sio.on('light')
+# def handle_light(sid, data):
+#     print('Received light status:', data)
+#     # Do something with the light status
+#     sio.emit('light_status', {'status': 'ok'})
+
+
+# @sio.on('temperature')
+# def handle_temperature(temp_status):
+#     print('Received temperature status:', temp_status)
+
+
+# @sio.event
+# def disconnect(sid):
+#     print(sid, 'disconnected')
+
+
+# if __name__ == '__main__':
+#     # Run the app on port 5000
+#     eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5000)), app)
